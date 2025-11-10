141b90feab69ef2a1dc4cc9c31ff05e66ffbdd64
k0r37k1 <64130853+k0r37k1@users.noreply.github.com>
2025-11-10 23:40:17 +0100

    test(installer): add WebServerController API endpoint tests (RED)

--- tests/Feature/Installer/WebServerControllerTest.php
+++ tests/Feature/Installer/WebServerControllerTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Installer;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\File;
 use Tests\TestCase;
 
@@ -20,8 +19,6 @@ use Tests\TestCase;
  */
 class WebServerControllerTest extends TestCase
 {
-    use RefreshDatabase;
-
     protected string $htaccessPath = '';
 
     protected string $nginxPath = '';
@@ -55,5 +52,91 @@ class WebServerControllerTest extends TestCase
         parent::tearDown();
     }
 
-    // Tests will be added in next tasks
+    public function testPostApacheConfigCreatesHtaccessReturns201(): void
+    {
+        $response = $this->postJson('/installer/webserver/apache', [
+            'force_https' => true,
+            'enable_compression' => true,
+            'enable_caching' => true,
+        ]);
+
+        $response->assertStatus(201);
+        $response->assertJsonStructure([
+            'success',
+            'path',
+            'message',
+            'content',
+        ]);
+        $response->assertJson([
+            'success' => true,
+        ]);
+
+        $this->assertFileExists($this->htaccessPath);
+    }
+
+    public function testPostApacheConfigReturns409WhenFileExists(): void
+    {
+        // Create existing .htaccess file
+        File::put($this->htaccessPath, '# Existing file');
+
+        $response = $this->postJson('/installer/webserver/apache', [
+            'force_https' => true,
+        ]);
+
+        $response->assertStatus(409);
+        $response->assertJson([
+            'success' => false,
+        ]);
+    }
+
+    public function testPostApacheConfigValidatesBooleanParameters(): void
+    {
+        $response = $this->postJson('/installer/webserver/apache', [
+            'force_https' => 'not-a-boolean',
+            'enable_compression' => 123,
+            'enable_caching' => [],
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['force_https', 'enable_compression', 'enable_caching']);
+    }
+
+    public function testGetDetectReturnsServerType(): void
+    {
+        $response = $this->getJson('/installer/webserver/detect');
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'success',
+            'server_type',
+            'server_software',
+        ]);
+        $response->assertJson([
+            'success' => true,
+        ]);
+
+        // Server type should be one of: apache, nginx, unknown
+        $this->assertContains(
+            $response->json('server_type'),
+            ['apache', 'nginx', 'unknown']
+        );
+    }
+
+    public function testPostNginxConfigCreatesExampleReturns201(): void
+    {
+        $response = $this->postJson('/installer/webserver/nginx');
+
+        $response->assertStatus(201);
+        $response->assertJsonStructure([
+            'success',
+            'path',
+            'message',
+            'content',
+        ]);
+        $response->assertJson([
+            'success' => true,
+        ]);
+
+        $this->assertFileExists($this->nginxPath);
+    }
 }
