951c0aa9e447ed95db4781ed9238b3d50fae5f50
k0r37k1 <64130853+k0r37k1@users.noreply.github.com>
2025-11-10 23:19:17 +0100

    test(installer): add SSL detection tests (RED)

--- tests/Unit/Services/Installer/WebServerConfigGeneratorTest.php
+++ tests/Unit/Services/Installer/WebServerConfigGeneratorTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\Unit\Services\Installer;
 
 use App\Services\Installer\WebServerConfigGenerator;
+use Illuminate\Http\Request;
 use Tests\TestCase;
 
 class WebServerConfigGeneratorTest extends TestCase
@@ -17,5 +18,48 @@ class WebServerConfigGeneratorTest extends TestCase
         $this->generator = new WebServerConfigGenerator;
     }
 
-    // Tests will be added in next tasks
+    public function testDetectSslEnabledReturnsTrueWhenHttps(): void
+    {
+        $request = Request::create('https://example.com', 'GET');
+        $this->app->instance('request', $request);
+
+        $result = $this->invokeMethod($this->generator, 'detectSslEnabled');
+
+        $this->assertTrue($result);
+    }
+
+    public function testDetectSslEnabledReturnsFalseWhenHttp(): void
+    {
+        $request = Request::create('http://example.com', 'GET');
+        $this->app->instance('request', $request);
+
+        $result = $this->invokeMethod($this->generator, 'detectSslEnabled');
+
+        $this->assertFalse($result);
+    }
+
+    public function testForceHttpsManualOverrideRespected(): void
+    {
+        $request = Request::create('http://example.com', 'GET');
+        $this->app->instance('request', $request);
+
+        $content = $this->invokeMethod($this->generator, 'buildApacheHtaccess', [
+            ['force_https' => true],
+        ]);
+
+        $this->assertStringContainsString('RewriteCond %{HTTPS} !=on', $content);
+        $this->assertStringContainsString('https://%{HTTP_HOST}', $content);
+    }
+
+    /**
+     * Invoke a private or protected method
+     */
+    private function invokeMethod(object $object, string $methodName, array $parameters = []): mixed
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
 }
